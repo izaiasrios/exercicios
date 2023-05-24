3a6684d98a6a38eda0f7e75fe0ecc37fb90fda50
Izaias Rios <izaias.rios@gmail.com>
2023-05-24 07:09:51 -0300

    Aplicando JSX

---
+++ src/pages/basicos/jsx1.jsx
@@ -0,0 +1,10 @@
+// JSX
+export default function jsxDiferente(){
+    return (
+    <div>
+        
+    <h1>jsx #01</h1>
+
+    </div>
+    )
+}
---
+++ src/pages/basicos/jsx2.jsx
@@ -0,0 +1,7 @@
+// JSX
+export default function jsx2(){
+    return <div>       
+                <h1>jsx #02</h1>
+          </div>
+    
+}
